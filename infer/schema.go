0cd0c898d692d1d33362bc3cbbd825d0bd9f133c
Dominik Roos <roos@anapaya.net>
2023-11-07 11:52:33 +0100

    PATCH: support uuid.UUID

--- infer/schema.go
+++ infer/schema.go
@@ -19,6 +19,7 @@ import (
 	"reflect"
 	"strings"
 
+	"github.com/google/uuid"
 	"github.com/hashicorp/go-multierror"
 	"github.com/pulumi/pulumi/pkg/v3/codegen/schema"
 	"github.com/pulumi/pulumi/sdk/v3/go/common/resource"
@@ -116,6 +117,9 @@ func serializeTypeAsPropertyType(
 ) (schema.TypeSpec, error) {
 	for t.Kind() == reflect.Pointer {
 		t = t.Elem()
+	}
+	if t == reflect.TypeOf(uuid.UUID{}) {
+		return schema.TypeSpec{Type: "string", Plain: false}, nil
 	}
 	if t == reflect.TypeOf(resource.Asset{}) {
 		return schema.TypeSpec{
