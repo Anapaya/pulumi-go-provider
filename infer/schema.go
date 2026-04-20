596837a1bd367678132cad18c9e278d83a71e05c
Dominik Roos <roos@anapaya.net>
2026-04-20 11:52:28 +0200

    PATCH: support time.Time

--- infer/schema.go
+++ infer/schema.go
@@ -20,6 +20,7 @@ import (
 	"maps"
 	"reflect"
 	"strings"
+	"time"
 
 	"github.com/google/uuid"
 	"github.com/hashicorp/go-multierror"
@@ -133,6 +134,9 @@ func serializeTypeAsPropertyType(
 ) (schema.TypeSpec, error) {
 	for t.Kind() == reflect.Pointer {
 		t = t.Elem()
+	}
+	if t == reflect.TypeOf(time.Time{}) {
+		return schema.TypeSpec{Type: "string", Plain: false}, nil
 	}
 	if t == reflect.TypeOf(uuid.UUID{}) {
 		return schema.TypeSpec{Type: "string", Plain: false}, nil
